95ac17a2452e3b12dea18c2cf0f21d564c3f5a6f
Rares-Andrei Cotoi <62752604+coty1@users.noreply.github.com>
2025-01-12 20:15:36 +0200

    pracc 3

--- practice/netflix.sql
+++ practice/netflix.sql
@@ -36,3 +36,92 @@ CREATE OR ALTER TRIGGER TrOnUpdate
 UPDATE R
 SET C5 = 0
 WHERE FK1 = FK2
+
+-- part b
+create table Accounts(
+    AccountID INT PRIMARY KEY IDENTITY (1,1),
+    AccEmail VARCHAR(100) UNIQUE ,
+    AccJoinDate DATE,
+    AccActive BIT
+);
+
+create table Actors(
+    ActorID INT PRIMARY KEY IDENTITY (1,1),
+    ActorName VARCHAR(100),
+    ActorNationality VARCHAR(100)
+);
+
+create table Movies(
+    MovieID INT PRIMARY KEY IDENTITY (1,1),
+    MovieTitle VARCHAR(100),
+    MovieDuration INT, -- must be positive
+    CONSTRAINT MovieDuration CHECK (MovieDuration > 0)
+);
+
+create table Actors_Movies(
+    ActorID INT REFERENCES Actors(ActorID),
+    MovieID INT REFERENCES Movies(MovieID),
+    PRIMARY KEY (ActorID, MovieID)
+);
+
+create table Reviews(
+    ReviewID INT PRIMARY KEY IDENTITY (1,1),
+    ReviewDate DATE,
+    ReviewRating FLOAT,
+    ReviewText VARCHAR(100),
+    AccountID INT REFERENCES Accounts(AccountID),
+    MovieID INT REFERENCES Movies(MovieID),
+    CONSTRAINT ReviewRating CHECK (ReviewRating between 0 AND 10)
+);
+
+create table Watchlists(
+    WatchID INT PRIMARY KEY IDENTITY (1,1),
+    AccountID INT REFERENCES Accounts(AccountID),
+    MovieID INT REFERENCES Movies(MovieID),
+    WatchDate DATE
+);
+
+-- task b
+CREATE OR ALTER PROCEDURE sp_AddAccount (@ParamAcc INT, @ParamMovie INT) AS
+    BEGIN
+        IF NOT EXISTS(SELECT AccountID FROM Accounts WHERE AccountID = @ParamAcc)
+        BEGIN
+            PRINT 'Account is not valid'
+            RETURN
+        end
+        IF NOT EXISTS(SELECT MovieID FROM Movies WHERE MovieID = @ParamMovie)
+            BEGIN
+                PRINT 'Movie is not valid'
+                RETURN
+            end
+        -- verificare daca exista in watchlist?
+        INSERT INTO Watchlists(AccountID, MovieID, WatchDate) VALUES (@ParamAcc, @ParamMovie, GETDATE())
+    end
+    GO;
+
+-- task c
+CREATE VIEW vw_ShowNames AS
+SELECT * FROM Movies AS M
+         INNER JOIN
+     Reviews AS R ON M.MovieID = R.MovieID
+         INNER JOIN
+     (SELECT A.AccountID
+      FROM Accounts AS A
+               INNER JOIN
+           Watchlists AS W ON W.AccountID = A.AccountID
+      GROUP BY A.AccountID
+      HAVING COUNT(A.AccountID) > 3) A
+     ON A.AccountID = R.AccountID
+
+-- task d
+create function fn_NumberOfMovies(@S INT, @E INT, @R INT)
+    RETURNS INT
+    AS
+    BEGIN
+        DECLARE @RESULT INT = 0;
+        SELECT @RESULT = COUNT(*) FROM Movies M
+        WHERE (SELECT COUNT(*) from Watchlists W
+                               WHERE W.MovieID = M.MovieID AND W.WatchDate BETWEEN @S AND @E) >= @R;
+        RETURN @RESULT;
+    END
+    GO
